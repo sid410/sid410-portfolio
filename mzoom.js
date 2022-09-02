65c9c013c396c0898138bcedaf827f7902ae3f56
sid410 <butaslaciii@gmail.com>
2022-09-02 16:44:57 +0900

    medium zoom testing

--- mzoom.js
+++ mzoom.js
@@ -1,4 +1,12 @@
 mediumZoom('.zoom', {
     scrollOffset: 0,
     background: 'rgba(25, 18, 25, .9)',
+    container: {
+        width: 720,
+        height: 480,
+        top: 64,
+        bottom: 64,
+        right: 0,
+        left: 0,
+      },
 })
