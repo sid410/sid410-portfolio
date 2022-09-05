b0680e58ab6d97fe2cb0ee7e6b145c8753468d5f
sid410 <butaslaciii@gmail.com>
2022-09-05 18:32:36 +0900

    template facebook

--- mzoom.js
+++ mzoom.js
@@ -1,12 +1,24 @@
-mediumZoom('.zoom', {
-    scrollOffset: 0,
-    background: 'rgba(25, 18, 25, .9)',
-    container: {
-        width: 720,
-        height: 480,
-        top: 64,
-        bottom: 64,
-        right: 0,
-        left: 0,
-      },
-})
+const zoom = mediumZoom('#image1', {
+    background: 'rgba(0, 0, 0, 0.9)',
+    template: '#template-facebook',
+    container: '[data-zoom-container]',
+  });
+
+
+
+  // You can start manipulating the DOM after the \`opened\` event has been triggered
+  zoom.on('opened', () => {
+    const closeButton = document.querySelector('[data-zoom-close]');
+    closeButton.addEventListener('click', () => zoom.close());
+  });
+
+  // Block scroll on zoom
+  zoom.on('open', () => {
+    document.body.style.overflow = 'hidden';
+    document.getElementById("image1").style.opacity  = "0";
+  });
+
+  zoom.on('close', () => {
+    document.body.style.overflow = '';
+    document.getElementById("image1").style.opacity  = "1";
+  });
